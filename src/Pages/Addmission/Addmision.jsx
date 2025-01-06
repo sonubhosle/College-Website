e482747089b6f79579008cba49c1e751236cee94
sonubhosle <sbhosle1011@gmail.com>
2025-01-06 10:56:50 +0530

    first commit

---
+++ src/Pages/Addmission/Addmision.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Addmision = () => {
+  return (
+    <div>Addmision</div>
+  )
+}
+
+export default Addmision
---
+++ src/Pages/Courses/Courese.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Courese = () => {
+  return (
+    <div>Courese</div>
+  )
+}
+
+export default Courese
